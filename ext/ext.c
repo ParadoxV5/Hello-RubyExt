944f533596b526f6689eced9c834abeef29794e7
ParadoxV5 <paradox.ver5@gmail.com>
2023-06-14 19:10:05 -0600

    Write some Hello World in C

--- ext/ext.c
+++ ext/ext.c
@@ -1,2 +1,12 @@
 #include <ruby.h>
 
+static const char MyExt_greeting[] = "Hello from Ruby Extension!\n";
+
+static VALUE MyExt_rb_hello(__attribute__((unused)) VALUE klass) {
+  fputs(MyExt_greeting, stderr);
+  return rb_str_new_cstr(MyExt_greeting);
+}
+
+void Init_ext(void) {
+  rb_define_global_function("hello", MyExt_rb_hello, 0);
+}
